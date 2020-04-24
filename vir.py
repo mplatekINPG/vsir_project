f061cf42438a0801b1955c0433aedd1f6e972b75
Maciej Kleszcz <prezes702@gmail.com>
2020-04-24 12:23:36 +0200

    Dodanie wykrywania ruchu i kierunku

--- vir.py
+++ vir.py
@@ -1,15 +1,19 @@
 import cv2
-import numpy as np
+
 import time
 from imutils import grab_contours
+import collections
 import math
 
 
 class Circle:
-    def __init__(self, x, y, r):
+    def __init__(self, x, y, r, speed, direct):
         self.x = x
         self.y = y
         self.radius = r
+        self.speed = 0
+        self.directRadAv = 0
+        self.directRad = collections.deque(maxlen=10)
 
 # =============================================================================
 # Find green circles in image
@@ -48,7 +52,7 @@ def FindGreenCircles(frame):
             
        		# only proceed if the radius meets a minimum size
        		if radius > minRadiusPix:
-                   circles.append(Circle(int(x), int(y), int(radius)) )  
+                   circles.append(Circle(int(x), int(y), int(radius), 0, 0))  
 
     return circles
 
@@ -70,31 +74,90 @@ def AddCicrlesToFrame(frame, circles):
 # Arguments: frame - image, circles - list of cirles(objects of Circle class) 
 # return image with added text
 # =============================================================================
-def addCicrleText(frame, circles)  :
+def addCicrleText(frame, circles):
     if circles is not None:
         for c in circles: 
             text = "X:" + str(c.x) + " Y:" + str(c.y)
             cv2.putText(frame, text, (c.x+c.radius, c.y),cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
             text = "Radius:" + str(c.radius)
             cv2.putText(frame, text, (c.x+c.radius, c.y+30),cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            text = "Speed:" + str(round(c.speed, 2))
+            cv2.putText(frame, text, (c.x+c.radius, c.y+60),cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
     return frame
+
+
+def addDirectLines(frame, circles):
+    lenght = 1000
+    
+    if circles is not None:
+        for c in circles: 
+            if c.speed > 0:
+                x = int( round( c.x + lenght * math.sin(c.directRadAv),0 ) )
+                y = int( round( c.y + lenght * math.cos(c.directRadAv),0 ) )
+                frame = cv2.line(frame,(c.x,c.y),(x,y),(255,0,0),5)
+
+                
+            
+    return frame
+
+def findNeighbour(actual, surraund):
+    hmin = 100000
+    f = []
+    x = 0
+    y = 0
+    for c in surraund:
+        h = math.sqrt(((c.x - actual.x)**2) + ((c.y - actual.y)**2))
+        if h < hmin:
+            hmin = h
+            x = c.x
+            y = c.y   
+            f = c
+    return f,x,y,hmin
+        
+
+def calculateMove(buf):
+    last = buf[-1]
+
+    
+    if(len(buf)<2):
+        return last
+    
+    for c in last:
+        neig,x,y,h = findNeighbour(c, buf[-2])
+        c.speed = 0;
+        if x!=0 and y!=0 and h<1000 and h>10:
+            c.speed = h
+            c.directRad = neig.directRad            
+            rad = math.atan2( x-c.x, y-c.y) + math.pi
+            c.directRad.append(rad)
+            c.directRadAv = (sum(c.directRad) + len(c.directRad)*3.14 )/len(c.directRad) -3.14
+            
+            
+    
+    return last
+    
+
+
+
 
 
 #cap = cv2.VideoCapture("dev/video0")
 #cap = cv2.VideoCapture(0)
-cap = cv2.VideoCapture('test7.mp4')
+cap = cv2.VideoCapture('test8.mp4')
 if not cap.isOpened():
      print("Could not open camera")
      #cap.open("dev/video0")
      cap.open(0)
      
+
+detectBuf = collections.deque(maxlen=10)
      
 frame_width = int(cap.get(3))
 frame_height = int(cap.get(4))    
 fps = cap.get(5)
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi',fourcc, 60, (frame_width,frame_height)) 
-outm = cv2.VideoWriter('output.avi',fourcc, 60, (frame_width,frame_height)) 
+out = cv2.VideoWriter('output1.avi',fourcc, 60, (frame_width,frame_height)) 
+
 #out = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
 time.sleep(2.0)
      
@@ -109,9 +172,16 @@ while(True):
 
      
      circlesList = FindGreenCircles(frame)
-     frame = AddCicrlesToFrame(frame, circlesList)
-     frame = addCicrleText(frame, circlesList)
+     detectBuf.append(circlesList)
+     detectBuf[-1] = calculateMove(detectBuf)
      
+     frame = addDirectLines(frame, detectBuf[-1])
+     frame = AddCicrlesToFrame(frame, detectBuf[-1])
+     frame = addCicrleText(frame, detectBuf[-1])
+     
+     
+    
+     detectBuf.append(circlesList)
    
      cv2.imshow("detect", frame)
      
