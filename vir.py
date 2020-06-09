cd58dceb930ea8bcafaacdc2d50f4dd6977ef5c4
m Platek <matplatekk@gmail.com>
2020-06-09 23:42:04 +0200

    Update vir.py

--- vir.py
+++ vir.py
@@ -1,10 +1,8 @@
 import cv2
-
 import time
 from imutils import grab_contours
 import collections
 import math
-
 
 class Circle:
     def __init__(self, x, y, r, speed, direct):
@@ -20,7 +18,7 @@ class Circle:
 # =============================================================================
 # Find green circles in image
 # Argument: frame - image
-# return list of finded circles (type classCircle)
+# return list of finded circles (type class Circle)
 # =============================================================================
 def FindGreenCircles(frame):  
     #ball color range in HSV
@@ -43,15 +41,7 @@ def FindGreenCircles(frame):
     circles = list()
 
     for c in cnts: 
-       		# find the largest contour in the mask, then use
-       		# it to compute the minimum enclosing circle and
-       		# centroid
-       		#c = max(cnts, key=cv2.contourArea)
-            
        		((x, y), radius) = cv2.minEnclosingCircle(c)
-       		# M = cv2.moments(c)
-       		# center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-            
        		# only proceed if the radius meets a minimum size
        		if radius > minRadiusPix:
                    circles.append(Circle(int(x), int(y), int(radius), 0, 0))  
@@ -60,7 +50,7 @@ def FindGreenCircles(frame):
 
 # =============================================================================
 # Drawing circle on image
-# Arguments: frame - image, circles - list cirles to drawn (objects of Circle class) 
+# Arguments: frame - image, circles - list circles to draw (objects of Circle class) 
 # return image with cirles
 # =============================================================================
 def AddCicrlesToFrame(frame, circles):
@@ -73,7 +63,7 @@ def AddCicrlesToFrame(frame, circles):
     
 # =============================================================================
 # Add position and size of cirle to screen
-# Arguments: frame - image, circles - list of cirles(objects of Circle class) 
+# Arguments: frame - image, circles - list of circles (objects of Circle class) 
 # return image with added text
 # =============================================================================
 def addCicrleText(frame, circles):
@@ -87,7 +77,11 @@ def addCicrleText(frame, circles):
             cv2.putText(frame, text, (c.x+c.radius, c.y+60),cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
     return frame
 
-
+# =============================================================================
+# Add direction lines of moving circle to screen
+# Arguments: frame - image, circles - list of circles (objects of Circle class) 
+# return image with added lines
+# =============================================================================
 def addDirectLines(frame, circles):
     lenght = 1000
     
@@ -96,12 +90,15 @@ def addDirectLines(frame, circles):
             if c.speed > 0:
                 x = int( round( c.x + lenght * math.sin(c.directRadAv),0 ) )
                 y = int( round( c.y + lenght * math.cos(c.directRadAv),0 ) )
-                frame = cv2.line(frame,(c.x,c.y),(x,y),(255,0,0),5)
-
-                
-            
+                frame = cv2.line(frame,(c.x,c.y),(x,y),(255,0,0),5)       
     return frame
 
+# =============================================================================
+# Find a nearest circle from surrounding circles
+# Arguments: actual - circle (objects of Circle class) in actual frame of video, 
+#            surround - list of previously found cicrles (objects of Circle class) 
+# return nearest neighbour
+# =============================================================================
 def findNeighbour(actual, surraund):
     hmin = 100000
     f = []
@@ -116,11 +113,13 @@ def findNeighbour(actual, surraund):
             f = c
     return f,x,y,hmin
         
-
+# =============================================================================
+# Calculates speed and direction of circle based on previous and actual position
+# Arguments: buf - list of found circles (bjects of Circle class) in actual and previous images 
+# return list of circles (object of Circle class) with updated parameters
+# =============================================================================
 def calculateMove(buf):
     last = buf[-1]
-
-    
     if(len(buf)<2):
         return last
     
@@ -133,20 +132,19 @@ def calculateMove(buf):
             rad = math.atan2( x-c.x, y-c.y) + math.pi
             c.directRad.append(rad)
             c.directRadAv = (sum(c.directRad) + len(c.directRad)*3.14 )/len(c.directRad) -3.14
-            
-            
-            
-    
     return last
 
+# =============================================================================
+# Detects if collision is about to happen and returns where
+# Arguments: buf - list of found circles (bjects of Circle class) in actual and previous images
+# return flag if collision is going to happen, point of collision, 
+#       centers of Circle class objects that will hit and angle
+# =============================================================================
 def detectCollision(buf):
-    
     if(len(buf)<2):
         return False, 0, 0, 0, 0, 0, 0, 0
-    
     bufSize = len(buf);
   
-    
     for s in range(1, 300):
         for i in range(0, bufSize):
             mult = s/10;
@@ -169,8 +167,9 @@ def detectCollision(buf):
 
 
 
-
-
+# =============================================================================
+# Source initialization
+# =============================================================================
 #cap = cv2.VideoCapture("dev/video0")
 #cap = cv2.VideoCapture(0)
 cap = cv2.VideoCapture('test9.mp4')
@@ -179,18 +178,30 @@ if not cap.isOpened():
      #cap.open("dev/video0")
      cap.open(0)
      
-
+# =============================================================================
+# Detected circles list initialization
+# =============================================================================
 detectBuf = collections.deque(maxlen=10)
      
+# =============================================================================
+# Video parameters reading
+# =============================================================================
 frame_width = int(cap.get(3))
 frame_height = int(cap.get(4))    
 fps = cap.get(5)
+
+# =============================================================================
+# Result video file initialization
+# =============================================================================
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 out = cv2.VideoWriter('output.avi',fourcc, 60, (frame_width,frame_height)) 
 
 #out = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
 time.sleep(2.0)
      
+# =============================================================================
+# Main processing loop
+# =============================================================================
 while(True):
      #capturing frames
      ret, frame = cap.read()
@@ -199,37 +210,37 @@ while(True):
      if frame is None:
          print("Invalid frame")
          break
-
      
+     #finding circles
      circlesList = FindGreenCircles(frame)
+     #updating buffer
      detectBuf.append(circlesList)
      detectBuf[-1] = calculateMove(detectBuf)
      
+     #draw circles, lines, write text
      frame = addDirectLines(frame, detectBuf[-1])
      frame = AddCicrlesToFrame(frame, detectBuf[-1])
      frame = addCicrleText(frame, detectBuf[-1])
      
-     
-     
-     
+     #calculate collision
      flag, x, y,x1,y1, x2, y2, angle = detectCollision(detectBuf[-1])
      if(flag):
+         #draw effects of collision
          cv2.circle(frame, (round(x), round(y)), 5, (0, 0, 255), -1)
          cv2.line(frame,(round(x1), round(y1)),(round( x1 + 500 * math.sin(angle)),round( y1 + 500 * math.cos(angle))),(255,0,255),3)
          cv2.line(frame,(round(x2), round(y2)),(round( x2 + 500 * math.sin(angle-math.pi/2)),round( y2 + 500 * math.cos(angle-math.pi/2))),(255,0,255),3)
-     
-    
-     detectBuf.append(circlesList)
    
-     cv2.imshow("detect", frame)
+     detectBuf.append(circlesList)
      
+     #show final video frame
+     cv2.imshow("detect", frame)
+     #save the frame to file
      out.write(frame)
+        
      #finish when Q is pressed
      if cv2.waitKey(1) & 0xFF == ord("q"):
          break
      
-
-
 cap.release()
 out.release()
 cv2.destroyAllWindows()
